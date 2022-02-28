ae7a3a88b47e2112c6ad914b2d249624a4057e3d
Elijla Khalid <elijlakhalid99@gmail.com>
2022-02-28 12:59:00 +0200

    Done I

--- Solid/Converters/BinaryConverter.cs
+++ Solid/Converters/BinaryConverter.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Solid.Converters
 {
-    public class BinaryConverter : Converter
+    public class BinaryConverter : Converter, IConverter
     {
         public BinaryConverter(int decimalNumber) : base(decimalNumber)
         {
@@ -15,6 +15,31 @@ namespace Solid.Converters
         public override string Convert()
         {
             return $"The result is: {System.Convert.ToString(DecimalNumber, 2)}";
+        }
+
+        public string BinaryToText(string binary)
+        {
+            binary = binary.Replace(" ", "");
+            var list = new List<Byte>();
+
+            foreach (var item in list)
+            {
+            }
+        }
+
+        public string HexadecimalToText(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TextToBinary(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TextToHexadecimal(string text)
+        {
+            throw new NotImplementedException();
         }
     }
 }
